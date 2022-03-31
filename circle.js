cebe796b9ba4b92ea8cbba90cd8dad0b4220a0ca
Richard Kondla <richard.kondla@stud.hs-hannover.de>
2022-03-31 13:31:53 +0200

    Circle Definition animation

--- circle.js
+++ circle.js
@@ -43,36 +43,97 @@ function init() {
     gl.attachShader(program, fragShader)
     gl.linkProgram(program)
 
-    //const vertices = generateCircleVertices(0.0, 0.0, 0.5, 0.1)
-    const vertices = generateHoleCircleVertices(0.1, 0.5, 0.1)
+    // const vertices = generateCircleVertices(0.0, 0.0, 0.5, 0.1)
 
-    const vbo = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
-    gl.bufferData(gl.ARRAY_BUFFER,
-        new Float32Array(vertices),
-        gl.STATIC_DRAW)
+    // const vertices = generateHoleCircleVertices(0.1, 0.5, 0.1)
+
+    // const vbo = vboSetup(gl, vertices)
+
+    // gl.clearColor(0.0, 1.0, 0.8, 1.0)
+    // gl.clear(gl.COLOR_BUFFER_BIT)
+
+    // gl.useProgram(program)
+    // gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
+    
+    // feedDataToShader(gl, program, "vertPos")
+    
+    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length / 2)
+
+    var circleDefinition = 1
+    var step = 0.002
+
+    function loop() {
+        const vertices = generateHoleCircleVertices(0.1, 0.5, circleDefinition)
+        const vbo = vboSetup(gl, vertices)
+
+        gl.clearColor(0.0, 1.0, 0.8, 1.0)
+        gl.clear(gl.COLOR_BUFFER_BIT)
+
+        gl.useProgram(program)
+        gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
+        feedDataToShader(gl, program, "vertPos")
+
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length / 2)
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, null)
+        
+        // higher def and slow down towards end 
+        if (circleDefinition > 0.1) {
+            circleDefinition -= step
+            if (step > 0.0004 && circleDefinition < 0.5) {
+                step -= 0.00001
+            }
+            requestAnimationFrame(loop)
+        }
+    }
+
+    requestAnimationFrame(loop)
+
     gl.bindBuffer(gl.ARRAY_BUFFER, null)
+}
 
-    gl.clearColor(0.0, 1.0, 0.8, 1.0)
-    gl.clear(gl.COLOR_BUFFER_BIT)
+function vboSetup(context, vertices) {
+    let vbo = context.createBuffer()
+    context.bindBuffer(context.ARRAY_BUFFER, vbo)
+    context.bufferData(context.ARRAY_BUFFER,
+        new Float32Array(vertices),
+        context.STATIC_DRAW)
+    context.bindBuffer(context.ARRAY_BUFFER, null)
+    return vbo
+}
 
-    gl.useProgram(program)
-    gl.bindBuffer(gl.ARRAY_BUFFER, vbo)
-
-    const positionAttributeLocation = gl.getAttribLocation(program, "vertPos")
-    gl.vertexAttribPointer(
+function feedDataToShader(context, program, shaderAttrName) {
+    const positionAttributeLocation = context.getAttribLocation(program, shaderAttrName)
+    context.vertexAttribPointer(
         positionAttributeLocation,
         2,
-        gl.FLOAT,
-        gl.FALSE,
+        context.FLOAT,
+        context.FALSE,
         2 * Float32Array.BYTES_PER_ELEMENT,
         0 * Float32Array.BYTES_PER_ELEMENT
     )
-    gl.enableVertexAttribArray(positionAttributeLocation)
+    context.enableVertexAttribArray(positionAttributeLocation)
+}
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length / 2)
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, null)
+function generateHoleCircleVertices(rInner, rOuter, step) {
+    const arr = []
+
+    for (let i = 0; i <= 2*Math.PI; i += step) {
+        let x = rInner * Math.cos(i)
+        let y = rInner * Math.sin(i)
+        
+        arr.push(x, y)
+        
+        x = rOuter * Math.cos(i)
+        y = rOuter * Math.sin(i)
+        
+        arr.push(x, y)
+    }
+    
+    arr.push(arr[0], arr[1], arr[2], arr[3])
+    
+    return arr
 }
 
 function generateCircleVertices(midX, midY, r, step) {
@@ -89,26 +150,6 @@ function generateCircleVertices(midX, midY, r, step) {
     // push the first value again to finish the circle
     arr.push(arr[2])
     arr.push(arr[3])
-
-    return arr
-}
-
-function generateHoleCircleVertices(rInner, rOuter, step) {
-    const arr = []
-
-    for (let i = 0; i <= 2*Math.PI; i += step) {
-        let x = rInner * Math.cos(i)
-        let y = rInner * Math.sin(i)
-
-        arr.push(x, y)
-
-        x = rOuter * Math.cos(i)
-        y = rOuter * Math.sin(i)
-
-        arr.push(x, y)
-    }
-
-    arr.push(arr[0], arr[1], arr[2], arr[3])
 
     return arr
 }
